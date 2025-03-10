391b436605f49838fb483eee461ed6492cf20137
Rishu <royadma0@gmail.com>
2025-03-10 11:45:12 +0530

    Mobile responsive

--- src/routers/api/observations/read.ts
+++ src/routers/api/observations/read.ts
@@ -4,8 +4,11 @@ import ServerError from "../../../utils/globals/error-handlers/ServerError";
 import GetReadQueryValidator from "../../../utils/validators/custom/read_query";
 
 const READ_OBSERVATION_HANDLER = CreateRequestHandler(async function () {
+    const projection = this.req.query.projection ? { __v: false, ...JSON.parse(String(this.req?.query?.projection)) } : { __v: false };
+
+
     this.send_data({
-        observations: await ObservationModel.find(this.local.filters, { __v: false }, { limit: this.local.limit, skip: this.local.limit * (this.local.page-1) }).sort({ date: -1 }),
+        observations: await ObservationModel.find(this.local.filters, projection, { limit: this.local.limit, skip: this.local.limit * (this.local.page-1) }).sort({ date: -1 }),
         total: await ObservationModel.countDocuments(this.local.filters),
     }, "Successfully fetched data.");
 }, [ServerError], undefined, [GetReadQueryValidator([10, 200])]);
